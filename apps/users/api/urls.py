beac7b8bf002f5568eab988b3dbb9e0fd6d65e8f
AshrayaBashyal <ashrayabashyal1@gmail.com>
2026-02-16 15:56:44 +0545

    add endpoints for signup, login and token refresh, change char encoding to UTF-8

--- apps/users/api/urls.py
+++ apps/users/api/urls.py
@@ -1,1 +1,11 @@
-ÿþ
+from django.urls import path
+from .views import SignupView, LoginView, RefreshTokenView
+
+
+app_name = 'users'
+
+urlpatterns = [
+    path('signup/', SignupView.as_view(), name='signup'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('login/refresh/', RefreshTokenView.as_view(), name='token_refresh'),
+]
