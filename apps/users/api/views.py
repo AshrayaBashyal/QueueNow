bb63607dc751884cdb31c04fc0f917c159113d04
AshrayaBashyal <ashrayabashyal1@gmail.com>
2026-02-16 14:33:53 +0545

    create gignup, login and reftesh_token views

--- apps/users/api/views.py
+++ apps/users/api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from .serializers import SignupSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView 
 
-# Create your views here.
+class SignupView(generics.CreateAPIView):
+    """Handles user registration"""
+    permission_classes = [permissions.AllowAny]
+    serializer_class = SignupSerializer
+
+
+class LoginView(TokenObtainPairView):
+    """Handles JWT token generation(Email/Password)"""
+    permission_classes = [permissions.AllowAny]
