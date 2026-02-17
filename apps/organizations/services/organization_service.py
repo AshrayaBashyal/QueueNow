01c543f73b5789c7c7cf455cf8895905a460d029
AshrayaBashyal <ashrayabashyal1@gmail.com>
2026-02-17 19:58:55 +0545

    add serivce for unique slug generation and implement it in organization creation service

--- apps/organizations/services/organization_service.py
+++ apps/organizations/services/organization_service.py
@@ -1,7 +1,26 @@
 from django.db import transaction
+from django.utils.text import slugify
 from ..models import Organization, Membership
 
+
 class OrganizationService:
+
+    @staticmethod
+    def _generate_unique_slug(name):
+        """
+        Creates a unique slug. 
+        Example: 'My Bank' -> 'my-bank', then 'my-bank-1', 'my-bank-2'...
+        """
+        base_slug = slugify(name)      
+        slug = base_slug
+        counter = 1
+
+        while Organization.objects.filter(slug=slug).exists():
+            slug = f"{base_slug}-{counter}"  
+            counter += 1
+        return slug   
+
+
     @staticmethod
     @transaction.atomic
     def create_organization(*, name, creator, **extra_fields):
@@ -9,8 +28,12 @@ class OrganizationService:
         Creates an organization and assigns the creator as the first ADMIN.
         Atomic transaction ensures we don't get an Org without an Admin.
         """
+
+        unique_slug = OrganizationService._generate_unique_slug(name)
+        
         organization = Organization.objects.create(
             name = name,
+            slug = unique_slug,
             created_by = creator,
             **extra_fields
         )        
