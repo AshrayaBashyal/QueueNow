d7703064dc29b7c7890f565df996c738cf213496
AshrayaBashyal <ashrayabashyal1@gmail.com>
2026-02-17 12:29:37 +0545

    create Invite model

--- apps/organizations/models.py
+++ apps/organizations/models.py
@@ -1,5 +1,6 @@
 import uuid
 from django.db import models
+from django.db.models import Q
 from django.conf import settings
 from django.utils.text import slugify
 from core.utils.text_cleaners import collapse_spaces
@@ -14,11 +15,7 @@ class Organization(models.Model):
     address = models.TextField(blank=True, null=True)
     phone_number = models.CharField(max_length=20, blank=True, null=True)
     
-    created_by = models.ForeignKey(
-        settings.AUTH_USER_MODEL, 
-        on_delete=models.PROTECT,
-        related_name="owned_organizations"
-    )
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, related_name="owned_organizations")
     
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -63,3 +60,39 @@ class Membership(models.Model):
 
     class Meta:
         unique_together = ("user", "organization") 
+
+
+
+class Invite(models.Model):
+    class Status(models.TextChoices):
+        PENDING = "PENDING", "Pending"
+        CANCELLED = "CANCELLED", "Cancelled" 
+        ACCEPTED = "ACCEPTED", "Accepted"
+        REJECTED = "REJECTED", "Rejected"
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    organization = models.ForeignKey(
+        Organization, 
+        on_delete=models.CASCADE, 
+        related_name="invites"
+    )
+    email = models.EmailField()
+    role = models.CharField(max_length=10, choices=Membership.Role.choices, default=Membership.Role.STAFF)
+    status = models.CharField(max_length=10, choices=Status.choices, default=Status.PENDING)
+    invited_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, related_name="sent_invites")
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+        constraints = [
+            models.UniqueConstraint(
+                fields = ["organization", "email"],
+                condition = models.Q(status=PENDING),
+                name = 'unique_active_invite_per_org'
+            )
+        ]
+         
+    def __str__(self):
+        return f"{self.email} -> {self.organization.name} ({self.status})"
