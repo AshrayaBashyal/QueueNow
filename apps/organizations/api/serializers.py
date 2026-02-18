bea0cd5a049640803aa1d98821a7b12eacb5f003
AshrayaBashyal <ashrayabashyal1@gmail.com>
2026-02-18 14:17:21 +0545

    update serializer to list user_role

--- apps/organizations/api/serializers.py
+++ apps/organizations/api/serializers.py
@@ -20,7 +20,19 @@ class OrganizationCreateSerializer(serializers.ModelSerializer):
 
 class OrganizationSerializer(serializers.ModelSerializer):
     """Used for returning the full organization data"""
+    user_role = serializers.SerializerMethodField()
     class Meta:
         model = Organization
-        fields = ['id', 'name', 'slug', 'description', 'address', 'phone_number', 'created_at']
+        fields = ['id', 'name', 'slug', 'description', 'address', 'phone_number', 'created_at', 'user_role']
         read_only_fields = ['id', 'slug', 'created_at']
+
+    def get_user_role(self, obj):
+        # This is how you "grab" the request inside a serializer
+        request = self.context.get('request') 
+        
+        if request and request.user.is_authenticated:
+            # Now you can use request.user just like in a view
+            membership = obj.memberships.filter(user=request.user).first()
+            return membership.role if membership else None
+        return None
+
