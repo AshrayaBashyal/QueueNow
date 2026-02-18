19b146539c271e330e36c85ed696b84785b03160
AshrayaBashyal <ashrayabashyal1@gmail.com>
2026-02-18 15:56:44 +0545

    feat: implement flexible invite service with ID and email support

--- apps/organizations/services/invite_service.py
+++ apps/organizations/services/invite_service.py
@@ -9,24 +9,30 @@ class InviteService:
     
     @staticmethod
     @transaction.atomic
-    def send_invites(organization, invitee_email, inviter, role=Membership.Role.STAFF):
+    def send_invites(organization, inviter, invitee_email=None, invitee_id=None, role=Membership.Role.STAFF):
 
         if not inviter.memberships.filter(organization=organization, role=Membership.Role.ADMIN).exists():
             raise PermissionError("Only ADMIN can send invite")
-        try:
-            user_to_invite = User.objects.get(email=invitee_email)    
-        except User.DoesNotExist:
+        
+        user_to_invite = None
+        if invitee_id:
+            user_to_invite = User.objects.filter(id=invitee_id).first()
+        elif invitee_email:
+            user_to_invite = User.objects.filter(email=invitee_email).first() 
+
+        if not user_to_invite:
             raise ValueError("User must be registered to receive an invite.")
 
         if organization.memberships.filter(user=user_to_invite).exists():
             raise ValueError("User is already a member of this organization.")
                 
-        if Invite.objects.filter(organization=organization, email=invitee_email, status='PENDING').exists():
+        if Invite.objects.filter(organization=organization, invitee=user_to_invite , status='PENDING').exists():
             raise ValueError("An invitation is already pending for this user.")
 
         return Invite.objects.create(
             organization=organization,
-            email=invitee_email,
+            invitee=user_to_invite,
+            email=user_to_invite.email,
             invited_by=inviter,
             role=role
         )            
