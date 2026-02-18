1a4b2bdb7e511dde60f5fe5ade4165124fa3a4cc
AshrayaBashyal <ashrayabashyal1@gmail.com>
2026-02-18 13:51:58 +0545

    add organizations/my for retrieving organizations a user belongs to

--- apps/organizations/api/views.py
+++ apps/organizations/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework import status, viewsets
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.decorators import action
 from .serializers import OrganizationCreateSerializer, OrganizationSerializer
 from ..models import Organization
 from ..services.organization_service import OrganizationService
@@ -28,3 +29,12 @@ class OrganizationViewSet(viewsets.ModelViewSet):
 
         output_serializer = OrganizationSerializer(organization, context={'request': request})
         return Response(output_serializer.data, status=status.HTTP_201_CREATED)
+
+    @action(detail=False, methods=["get"], url_path="my") #detail=False --> /organizations/my/ (it acts on the whole collection).If detail=True --> /organizations/{slug}/my/ (acting on one specific org).
+    def my_organizations(self, request):
+        """
+        Returns only organizations where the current user is a member.
+        """
+        user_orgs = Organization.objects.filter(memberships__user = request.user)
+        serializer = self.get_serializer(user_orgs, many=True)
+        return Response(serializer.data)
